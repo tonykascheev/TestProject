b00530f514ab41370215b46529610d7e82c30aa6
a.kascheev <tony.kascheev@yandex.ru>
2021-02-20 11:56:17 +0300

    Изменил локаторы

--- src/test/java/PSB/TestDirectory/AdminRoleTest.java
+++ src/test/java/PSB/TestDirectory/AdminRoleTest.java
@@ -5,6 +5,7 @@ import PSB.Settings.ConfProperties;
 import PSB.Settings.WebDriverSettings;
 import io.qameta.allure.Step;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.FindBy;
 
 public class AdminRoleTest extends WebDriverSettings {
@@ -13,7 +14,7 @@ public class AdminRoleTest extends WebDriverSettings {
     public void AdminCreationRole() {
         loginPage.Auth();
         documentsPage.onPage();
-        driver.navigate().refresh();
+        driver.findElement(By.xpath("//body/div[@id='app']/section[1]/nav[1]/section[1]/section[1]")).click();
         documentsPage.setAdminTab();
         adminRolePage.setRole();
         adminRolePage.addRole();
